a163cfe69f3ab4a1986b6b9f8248411455a101fe
thebyronc <thebyronc@gmail.com>
2020-01-16 10:36:10 -0800

    add theme colors

---
+++ src/style-utilities/variables.js
@@ -0,0 +1,16 @@
+export const colors = {
+  white: '#FFFFFF',
+  text: '#000000',
+  altText: '#eeeeee',
+  primaryColor: '#008833',
+  linkColor: '#049748',
+  background: '#ededed',
+  footerBackground: '#666666',
+};
+
+export const breakpoints = {
+  mobile: '576px',
+  tablet: '768px',
+  desktop: '992px',
+  ultraWideDesktop: '1200px'
+};
