192ad6803a4856e3b01895909aa6aaa8cc15a4bc
thebyronc <thebyronc@gmail.com>
2020-01-20 13:15:23 -0800

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/images/img/ic_platt-u.png`, // This path is relative to the root of the site.
       },
     },
+    `styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
